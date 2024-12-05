51412f3ce54f9383b3058d6654d3061275f1a1bf
uxejune <148020148+uxejune@users.noreply.github.com>
2024-12-05 09:50:13 +0900

    first commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const myMobileMenu = document.getElementById("my-mobile-header")
+
+function openMobileMenu() {
+    myMobileMenu.classList.add("open")
+}
+
+function closeMobileMenu() {
+    myMobileMenu.classList.remove("open")
+}
